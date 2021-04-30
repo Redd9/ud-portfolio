e8b70426c74fd4c607ac7dc0508e6d37ad49366f
Yuri Droha <el9fernando@gmail.com>
2021-04-30 17:32:45 +0300

    Up button + low scroll (-)

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,32 @@ counter.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 }); 
 
+
+// const up = window.scroll(function() {
+//     if ($(this).scrollTop() > 1600) {
+//         $('.pageup').fadeIn();
+//     } else {
+//         $('.pageup').fadeOut();
+//     }
+
+//     $("a[href^='#up']").click(function(){
+//         const _href = $(this).attr("href");
+//         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+//         return false;
+//     });
+// });
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#up']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
